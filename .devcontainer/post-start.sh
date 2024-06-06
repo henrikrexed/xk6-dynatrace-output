18da27be25da8e2955b3e549546903123e89c7a3
Adam Gardner <adam.gardner@dynatrace.com>
2024-06-06 13:20:32 +1000

    wait until dockerd starts

--- .devcontainer/post-start.sh
+++ .devcontainer/post-start.sh
@@ -1,3 +1,11 @@
+# Wait for dockerd to start
+until pids=$(pidof dockerd)
+do   
+    sleep 1
+done
+
+echo "DEBUG: Docker is running. Proceed."
+
 docker run \
     -e K6_DYNATRACE_URL=$DT_URL \
     -e K6_DYNATRACE_APITOKEN=$DT_K6_TOKEN \
